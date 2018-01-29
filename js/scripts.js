1712635a137960152757772a793507a2f37ab969
Masen Matthews and Ron Craig <masen@>
2018-01-29 14:06:22 -0800

    JS function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
-// biz logic
-  function Song(mood, vibe) {
-    this.mood = mood;
-    this.vibe = vibe;
-  }
+// // biz logic
+//   function Song(mood, vibe) {
+//     this.mood = mood;
+//     this.vibe = vibe;
+//   }
 
 //mood
 var chillBar = [
@@ -30,25 +30,35 @@ var partyAlone = [
   "Song 12",
 ];
 
-var chillBar = chillBar[Math.floor(Math.random()*chillBar.length)];
-var partyBar = partyBar[Math.floor(Math.random()*partyBar.length)];
-var chillAlone = chillAlone[Math.floor(Math.random()*chillAlone.length)];
-var partyAlone = partyAlone[Math.floor(Math.random()*partyAlone.length)];
+var chillBarRandom = chillBar[Math.floor(Math.random()*chillBar.length)];
+var partyBarRandom = partyBar[Math.floor(Math.random()*partyBar.length)];
+var chillAloneRandom = chillAlone[Math.floor(Math.random()*chillAlone.length)];
+var partyAloneRandom = partyAlone[Math.floor(Math.random()*partyAlone.length)];
 
-var randomSong = function() {
-  if ("input#mood" === chill && "input#vibe" === bar) {
-    return ("You should sing " + chillBar + "!");
-  } else if ("input#mood" === chill && "input#vibe" === alone) {
-    return ("You should sing " + chillAlone + "!");
-  } else if ("input#mood" === party && "input#vibe" === bar) {
-    return ("You should sing " + partyBar + "!");
-  } else if ("input#mood" === party && "input#vibe" === alone) {
-    return ("You should sing " + partyAlone + "!");
-    else {
+
+var randomSong = function(anyMood, anyVibe) {
+  if (anyMood === "chill" && anyVibe === "bar") {
+    return ("You should sing " + chillBarRandom + "!");
+  } else if (anyMood === "chill" && anyVibe === "alone") {
+    return ("You should sing " + chillAloneRandom + "!");
+  } else if (anyMood === "party" && anyVibe === "bar") {
+    return ("You should sing " + partyBarRandom + "!");
+  } else if (anyMood === "party" && anyVibe === "alone") {
+    return ("You should sing " + partyAloneRandom + "!");
+  } else {
+    return("not working");
   }
-}
+
+};
 
 //ui logic
 $(document).ready(function() {
-  
+  $("button#show").click(function() {
+    var moodSelection = $("select#mood").val();
+    var vibeSelection = $("select#vibe").val();
 
+    var result = randomSong(moodSelection, vibeSelection)
+
+    $("#song-results").text(result);
+  });
+});
