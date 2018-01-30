17d69479ba1d147cc207d0bb6e307287192e88fb
Kaila Goff and Maggie Summers <maggie@>
2018-01-30 09:09:45 -0800

    add song object and score prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,84 @@
 // biz logic
-  function Song(name, artist, album) {
+  function Song(name, artist, album, score) {
     this.songName = name;
     this.artist = artist;
     this.album = album;
-    this.category = category;
+    this.score = score;
   }
 
 // var chillBar = [];
 
-var chillBar = [
-  "Drake: Hotline Bling",
-  "Dolly Parton: 9 to 5",
-  "Carrie Underwood: Before He Cheats",
-];
+// var chillBar = [
+//   "Drake: Hotline Bling",
+//   "Dolly Parton: 9 to 5",
+//   "Carrie Underwood: Before He Cheats",
+// ];
+//
+// var chillAlone = [
+//   "Jason Mraz: I'm Yours",
+//   "Frank Sinatra: My Way",
+//   "Fleetwood Mac: Go Your Own Way",
+// ];
+//
+// var partyBar = [
+//   "The Commodores: Brickhouse",
+//   "Young MC: Bust A Move",
+//   "Miley Cyrus: Party in the USA",
+// ];
+//
+// var partyAlone = [
+//   "Sia: Chandelier",
+//   "Don McLean: American Pie",
+//   "Frank Sinatra: My Way",
+// ];
 
-var chillAlone = [
-  "Jason Mraz: I'm Yours",
-  "Frank Sinatra: My Way",
-  "Fleetwood Mac: Go Your Own Way",
-];
 
-var partyBar = [
-  "The Commodores: Brickhouse",
-  "Young MC: Bust A Move",
-  "Miley Cyrus: Party in the USA",
-];
+// var allSongs = [
+//   "Song1", "Song2", "Song3", "Song4", "Song5", "Song6", "Song7", "Song8", "Song9", "Song10", "Song11", "Song12",
+// ];
+//
+// var chillBarRandom = chillBar[Math.floor(Math.random()*chillBar.length)];
+// var partyBarRandom = partyBar[Math.floor(Math.random()*partyBar.length)];
+// var chillAloneRandom = chillAlone[Math.floor(Math.random()*chillAlone.length)];
+// var partyAloneRandom = partyAlone[Math.floor(Math.random()*partyAlone.length)];
 
-var partyAlone = [
-  "Sia: Chandelier",
-  "Don McLean: American Pie",
-  "Frank Sinatra: My Way",
-];
-
-
-var allSongs = [
-  "Song1", "Song2", "Song3", "Song4", "Song5", "Song6", "Song7", "Song8", "Song9", "Song10", "Song11", "Song12",
-];
-
-// var randomSong = function() {
-//   chillBar[Math.floor(Math.random()*chillBar.length)];
-// }
-
-var chillBarRandom = chillBar[Math.floor(Math.random()*chillBar.length)];
-var partyBarRandom = partyBar[Math.floor(Math.random()*partyBar.length)];
-var chillAloneRandom = chillAlone[Math.floor(Math.random()*chillAlone.length)];
-var partyAloneRandom = partyAlone[Math.floor(Math.random()*partyAlone.length)];
-
-var songRecommendation = function(anyMood, anyVibe) {
-  if (anyMood === "chill" && anyVibe === "bar") {
-    return ("You should sing " + chillBarRandom + "!");
-  } else if (anyMood === "chill" && anyVibe === "alone") {
-    return ("You should sing " + chillAloneRandom + "!");
-  } else if (anyMood === "party" && anyVibe === "bar" ) {
-    return ("You should sing " + partyBarRandom + "!");
-  } else if (anyMood === "party" && anyVibe === "alone") {
-    return ("You should sing " + partyAloneRandom + "!");
+Song.prototype.songRecommendation = function(score) {
+  if (this.score < 5) {
+    alert(this.score);
+    return ("You should sing!");
+  } else if (this.score > 5) {
+    return ("Sing!");
+  } else if (this.score >= 10) {
+    return ("Please sing!");
   } else {
     return("Tell us your mood and vibe and try again.");
   }
 };
 
+
+
 //ui logic
 $(document).ready(function() {
   $("button#show").click(function() {
-    var moodSelection = $("select#mood").val();
-    var vibeSelection = $("select#vibe").val();
+    var moodSelection = parseInt($("select#mood").val());
+    var vibeSelection = parseInt($("select#vibe").val());
+    console.log(vibeSelection);
+    var drunkSelection = parseInt($("select#intox").val());
 
-    var result = songRecommendation(moodSelection, vibeSelection)
+    var newScore = (moodSelection + vibeSelection + drunkSelection);
+    console.log(newScore);
 
+    var newSong = new Song(moodSelection, vibeSelection, drunkSelection, newScore);
+    console.log(newSong);
+
+    var result = newSong.songRecommendation(newScore);
+    console.log(result);
     $("#song-results").text(result);
 
-    var reset = function () {
+    var reset = function() {
       $("select#mood").val("");
       $("select#vibe").val("");
+      $("select#intox").val("");
     }
 
     reset();
